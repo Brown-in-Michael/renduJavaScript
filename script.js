f8ba492aacefb46ed1c655ee8873a9afdab760af
Brown-in-Michael <MICHAEL.BROWN@latoile.em-lyon.com>
2025-12-16 16:01:37 +0100

    Time for CSS

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ fetch(JSON_LINK)
         // head
         let Header = document.getElementById("head");
         // avantages
-        let ADvantages = document.getElementById("adventages");
+        let ADvantages = document.getElementById("avantages");
         // produits
-        let Products = document.getElementById("products");
+        let Activities = document.getElementById("events");
         // services
         let Services = document.getElementById("services");
         // Témoinages
@@ -26,18 +26,23 @@ fetch(JSON_LINK)
             // Pour le div
             const newDiv = document.createElement("div");
             // Nos element dans le div
+            // title
             const insertH1 = document.createElement("h1");
-            insertH1.textContent = dat
+            insertH1.textContent = data.nomCommercial;
+            // paragraphe
             const insertp = document.createElement("p");
+            insertp.textContent = data.phraseAccroche;
             // div pour les button
             const divButtons = document.createElement("div");
             // button buy
-            const buybutton = document.createElement("a");
+            const buyButton = document.createElement("a");
+            buyButton.textContent = data.texteAppelAction;
             // button explore
-            const explorebutton = document.createElement("a");
+            const exploreButton = document.createElement("a");
+            exploreButton.textContent = "Explorer";
             // Etape 5: On inject les element using appendchild
-            divButtons.appendChild(buybutton);
-            divButtons.appendChild(explorebutton);
+            divButtons.appendChild(buyButton);
+            divButtons.appendChild(exploreButton);
 
             newDiv.appendChild(insertH1);
             newDiv.appendChild(insertp);
@@ -46,8 +51,99 @@ fetch(JSON_LINK)
             Header.appendChild(newDiv);
             Header.appendChild(divButtons);
 
+            // Partie Avantages
+            //  Etape 7:Créé le partie avantages
+
+            //  Créé un container 
+            const containerAvantages = document.createElement("div");
+            // Add a class
+            containerAvantages.classList.add("container-avantages");
+
+            // div pour le strong, h2, p
+            const divtexts = document.createElement("div");
+            let strongtext = document.createElement("strong");
+            strongtext.textContent = "Pourquoi";
+
+            let avantagesTitle = document.createElement("h2");
+            avantagesTitle.textContent = "Ce qui nous définit";
+
+            let advantagesParagraph = document.createElement("p");
+            advantagesParagraph.textContent = "Qualité sans égale dans chaque détail";
+            
+            divtexts.appendChild(strongtext);
+            divtexts.appendChild(avantagesTitle);
+            divtexts.appendChild(advantagesParagraph);
+            containerAvantages.appendChild(divtexts);
+
+            // Un div pour les cartes
+            const cardDiv = document.createElement("div");
+            cardDiv.classList.add("container-cards");
+
+            // Loop pour les cartes
+
+            data.avantagesClients.forEach(element => {
+                let cards = document.createElement("div"); 
+                cards.textContent = element;
+                cardDiv.appendChild(cards);
+
+            });
+            // Affichier le
+            ADvantages.appendChild(containerAvantages);
+            ADvantages.appendChild(cardDiv);
+
+
+            // Etape 8: Créé le partie Events (Activité)
+            const pourEvents = document.createElement("div");
+            pourEvents.classList.add("card-container");
+            // Use for each pour les event cards
+            data.activites.forEach(elementDeux => {
+                // créé un variable pour event titre
+                let nomActivite = document.createElement("h4");
+                nomActivite.textContent = elementDeux.nom;
+                // description
+                let descActivite = document.createElement("p");
+                descActivite.textContent = elementDeux.description;
+                // images
+                let images = document.createElement("img");
+                images.classList.add("eventImages")
+                images.src = elementDeux["image-url"];
+                // Affichier
+                pourEvents.appendChild(images);
+                pourEvents.appendChild(nomActivite);
+                pourEvents.appendChild(descActivite);
+                
+            });
+            Activities.appendChild(pourEvents);
+            // test
+            console.log(Activities);
+
+            // Etape 9: Créé le partie Témoinage (Avis)
+            const pourAvis = document.createElement("div")
+            pourAvis.classList.add("reviews-container")
+
+            data.temoignages.forEach(elementTrois => {
+                // prénom
+                let names = document.createElement("h4");
+                names.textContent = elementTrois.prenom;
+                // Experience
+                let experiences = document.createElement("p");
+                experiences.textContent = elementTrois.typeExperience;
+                // Avis
+                let comment = document.createElement("p");
+                comment.textContent = elementTrois.commentaire;
+                // Grade
+                let review = document.createElement("span")
+                review.textContent = "Review: " + elementTrois.note + "/5";
+
+                // Affiche
+                pourAvis.appendChild(names);
+                pourAvis.appendChild(experiences);
+                pourAvis.appendChild(comment);
+                pourAvis.appendChild(review);
+            });
+            Avis.appendChild(pourAvis);
 
         
-        
+
     })
 
